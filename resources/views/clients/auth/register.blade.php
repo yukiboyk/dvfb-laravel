d3c55d3c626295d2c0ccba2645af81f1416fbe28
Le Manh <manhxhien@gmail.com>
2022-06-08 22:13:48 +0700

    update2

--- resources/views/clients/auth/register.blade.php
+++ resources/views/clients/auth/register.blade.php
@@ -144,8 +144,8 @@
                         <div class="text-center">
                             <script>
                                 document.write(new Date().getFullYear())
-                            </script> Velzon. Crafted with <i
-                                    class="mdi mdi-heart text-danger"></i> by Themesbrand</p>
+                            </script> DVFB. Crafted with <i
+                                    class="mdi mdi-heart text-danger"></i> by YukiBoyK</p>
                         </div>
                     </div>
                 </div>
